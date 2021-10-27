7c230108d8f36341d180883c0975cb0de9f860ce
Aleksandrs Pirags <alpibit@hotmail.com>
2021-10-27 23:30:07 +0100

    Added lazy loading for images and worked on styling

--- src/components/about/Hero.jsx
+++ src/components/about/Hero.jsx
@@ -6,9 +6,8 @@ const About = () => {
   return (
     <section className={styles.heroWrap} id="hero">
       <img src={profilePhoto} className={styles.heroPhoto} alt=""/>
-      <h1 className={styles.heroHeader}>Lorem, ipsum dolor.</h1>
-      <p className={styles.heroText}>
-        Lorem ipsum dolor sit amet consectetur.
+      <h1 className={styles.heroHeader}>I'm Aleksandrs Pirags.</h1>
+      <p className={styles.heroText}>Self-taught front-end junior web developer. 
       </p>
     </section>
   );
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -8,25 +8,25 @@ import garageWeb from "../../images/garageweb.png";
 const Portfolio = () => {
   return (
     <section className={styles.wrap} id="portfolio">
-      <h1 className={styles.header}>Portfolio</h1>
+      <h1 className={styles.header}>Latest Work</h1>
       <div className={styles.portfolioWrap}>
         <div className={styles.project}>
-          <a target="_blank" href={config.WEATHERAPP}>
+          <a target="_blank" href={config.WEATHERAPP} className={styles.link}>
             <img src={weatherApp} className={styles.weatherApp} alt="" />
+            <p className={styles.description}>Weather App <br />(ReactJS and Redux)</p>
           </a>
-          <p>ReactJS and Redux</p>
         </div>
         <div className={styles.project}>
-          <a target="_blank" href={config.MAKEUPWEB}>
+          <a target="_blank" href={config.MAKEUPWEB} className={styles.link}>
             <img src={makeupWeb} className={styles.makeupWeb} alt="" />
+            <p className={styles.description}>Beauty Salon Website<br/>(HTML5 and CSS3)</p>
           </a>
-          <p>HTML5 and CSS3</p>
         </div>
         <div className={styles.project}>
-          <a target="_blank" href={config.GARAGEWEB}>
+          <a target="_blank" href={config.GARAGEWEB} className={styles.link}>
             <img src={garageWeb} className={styles.garageWeb} alt="" />
+            <p className={styles.description}>Local Garage Website<br/>(HTML5 and CSS3)</p>
           </a>
-          <p>HTML5 and CSS3</p>
         </div>
       </div>
     </section>
